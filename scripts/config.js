5c7a2244f91f26feb252679d0b7bccf8447a96bb
benjamin <benjaminverlaine@gmail.com>
2024-05-08 15:03:09 +0200

    change word

--- scripts/config.js
+++ scripts/config.js
@@ -1,10 +1,32 @@
 /*********************************************************************************
- * 
+ *
  * Ce fichier contient toutes les constantes nécessaires au fonctionnement du jeu.
  * En particulier les listes de mots et de phrases proposés à l'utilisateur
- * 
+ *
  *********************************************************************************/
 
 // Déclaration des tableaux contenant les listes des mots proposés à l'utilisateur
-const listeMots = ["Cachalot", "Pétunia", "Serviette"]
-const listePhrases = ["Pas de panique !", "La vie, l'univers et le reste", "Merci pour le poisson"]
+const listeMots = [
+    "ara",
+    "méduse",
+    "tigre",
+    "thon",
+    "papillon",
+    "tatou",
+    "carpe",
+    "ours",
+    "faucon",
+    "cochon",
+];
+const listePhrases = [
+    "C'est extrêmement rare qu'elle s'emporte.",
+    "un style d'architecture propre à l'époque.",
+    "Elle m'a assuré de son soutien.",
+    "Patrick travaillait dans une ferme voisine.",
+    "Il s'est livré à ses baisers.",
+    "l'assouplissement des restrictions de voyage",
+    "des hommes d'esprit militaire",
+    "victimes de racisme/discrimination",
+    "J'ai emprunté un appareil photo à Alex.",
+    "C'était peut-être une blague.",
+];
